1bd579043079aec289c64c135bfd364223ed06fc
sumi419 <sasgar04@gmail.com>
2019-02-16 17:50:29 -0800

    Add new Todo ➕

--- test/src/App.vue
+++ test/src/App.vue
@@ -4,6 +4,7 @@
     <!-- passing in a message prop to HelloWorld component -->
     <!-- {{msg}} -->
     <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
@@ -13,12 +14,15 @@
 // import HelloWorld from "./components/HelloWorld.vue";
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+
 export default {
   name: "app",
   components: {
     // HelloWorld
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -44,6 +48,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 };
@@ -67,5 +74,16 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
