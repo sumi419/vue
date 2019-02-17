0be6f543793494c2a2b63a4746efc17e42ba99d9
sumi419 <sasgar04@gmail.com>
2019-02-16 16:30:46 -0800

    Todo Item component added ✔️🎀

--- test/src/components/Todos.vue
+++ test/src/components/Todos.vue
@@ -2,14 +2,20 @@
   <div>
     <!-- loops through todos -->
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <h3>{{todo.title}}</h3>
+      <!-- <h3>{{todo.title}}</h3> -->
+      <!-- passing every todo from the for loop todos -->
+      <TodoItem v-bind:todo="todo"/>
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
 export default {
   name: "Todos",
+  components: {
+    TodoItem
+  },
   props: ["todos"]
 };
 </script>
