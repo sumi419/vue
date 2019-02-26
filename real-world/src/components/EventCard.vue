cadfd9793d72a89d0017bec7f9ed63a18ef2d1ea
sumi419 <sasgar04@gmail.com>
2019-02-26 15:25:16 -0800

    router moved

--- real-world/src/components/EventCard.vue
+++ real-world/src/components/EventCard.vue
@@ -1,4 +1,5 @@
 <template>
+  <router-link class="event-link" :to="{ name: 'event-show', params: {id: '1'} }">
   <div class="event-card -shadow">
     <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
     <h4 class="title">{{ event.title }}</h4>
