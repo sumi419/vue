80a33022b5d9361d956f19760ec56084a4dd2b4a
sumi419 <sasgar04@gmail.com>
2019-02-26 15:23:29 -0800

    event card styled added

--- real-world/src/components/EventCard.vue
+++ real-world/src/components/EventCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h4>{{title}}</h4>
+  <div class="event-card -shadow">
+    <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+    <h4 class="title">{{ event.title }}</h4>
+    <span>{{ event.attendees.length }}</span>
   </div>
 </template>
 
@@ -8,14 +10,45 @@
 export default {
   data() {
     return {
-      title: 'Park Cleanup'
+      event: {
+        id: 1,
+        title: 'Park Cleanup',
+        date: 'Tue Aug 19, 2018',
+        time: '6:00',
+        attendees: [
+          {
+            id: 'abc123',
+            name: 'Sumi A'
+          },
+          {
+            id: 'abc123',
+            name: 'Greg P'
+          }
+        ]
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-h4 {
-  color: pink;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
