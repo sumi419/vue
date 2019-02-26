c32820792558f21bfde10489b2fa35dc83b39d9b
sumi419 <sasgar04@gmail.com>
2019-02-26 10:08:39 -0800

    alternative way of writing router link

---
+++ real-world/src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1></h1>
+</template>
+<script>
+
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 
+    },
+  },
+  data() {
+    return {
+      quote: 'I'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- real-world/src/router.js
+++ real-world/src/router.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue"
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: "/",
       name: "home",
       component: Home
@@ -20,7 +20,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import( /* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
