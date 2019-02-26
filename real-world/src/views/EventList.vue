4a3b91ad825a20b7db730e14b634bab515dfc4e6
sumi419 <sasgar04@gmail.com>
2019-02-26 14:57:36 -0800

    Event Card component created

--- real-world/src/views/EventList.vue
+++ real-world/src/views/EventList.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <h1>Events Listing</h1>
+    <EventCard/>
     <router-link :to="{ name: 'event-show', params: {id: '1'} }">Show Event #1</router-link>
   </div>
 </template>
+
+import EventCard from '@/components/EventCard.vue'
+<script>
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
+
+<style>
+</style>
