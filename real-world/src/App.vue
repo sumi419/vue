5c72a781673a9a87d76ec2deb39ae771ef85193a
sumi419 <sasgar04@gmail.com>
2019-02-26 14:38:21 -0800

    new routes updated ✨

--- real-world/src/App.vue
+++ real-world/src/App.vue
@@ -3,8 +3,11 @@
     <div id="nav">
       <!-- <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>-->
-      <router-link :to="{ name: 'home' }">Home</router-link>|
-      <router-link :to="{ name: 'about' }">About</router-link>
+      <!-- <router-link :to="{ name: 'home' }">Home</router-link>|
+      <router-link :to="{ name: 'about' }">About</router-link>-->
+      <router-link :to="{ name: 'event-list' }">List</router-link>|
+      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link>|
+      <router-link :to="{ name: 'event-create' }">Create</router-link>
     </div>
     <router-view/>
   </div>
