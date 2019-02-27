85eb4035b0783a2cc2d0e18fd3d90a51270bf47f
sumi419 <sasgar04@gmail.com>
2019-02-26 16:15:16 -0800

    removed vue real world from this repo

--- real-world/src/views/EventCreate.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>create event</h1>
-</template>
--- real-world/src/views/EventShow.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <h1>Showing event #{{ id }}</h1>
-</template>
-<script>
-export default {
-  props: ['id']
-}
-</script>
